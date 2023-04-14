e263fdccd669652488cadbe30907c447efe66cc5
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-04-14 18:24:14 -0300

    almost working

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
-let wc = document.querySelectorAll('.woocommerce .products')
-let liList = []
+//let wc = document.querySelectorAll("#myTabContent .woocommerce")
+//let liList = []
+//let liArray = []
 wc.forEach((item, i) => {
     liList[i] = item.querySelectorAll('li') 
+    item.classList.add('carousel-inner')
+    item.classList.remove('woocommerce')
+    item.innerHTML = ''
+    liList[i].forEach(li => {
+        item.innerHTML += `<div class='carousel-item'${li.outerHTML}</div>`
+    })
 })
+
